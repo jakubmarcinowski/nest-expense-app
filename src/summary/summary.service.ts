d726621fea92f4f322438f76621065041377209b
jacob <j.jan.marcinowski@gmail.com>
2023-09-24 19:43:38 +0200

    updated structure

--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { ReportType } from 'src/data';
+import { ReportService } from 'src/report/report.service';
 
 @Injectable()
-export class SummaryService {}
+export class SummaryService {
+  constructor(private readonly reportService: ReportService) {}
+
+  calculateSummary() {
+    const allIncome = this.reportService.getAllReports(ReportType.INCOME);
+    const allExpense = this.reportService.getAllReports(ReportType.EXPENSE);
+    const totalIncome = allIncome.reduce((acc, cur) => acc + cur.amount, 0);
+    const totalExpense = allExpense.reduce((acc, cur) => acc + cur.amount, 0);
+    const netIncome = totalIncome - totalExpense;
+
+    return {
+      totalIncome,
+      totalExpense,
+      netIncome,
+    };
+  }
+}
